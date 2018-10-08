bb1178f1cfded0fe910d3d4848abf71b659512d4
alumno Secundaria <al3201@cedi.local>
2018-10-08 09:47:19 -0500

    Agregar boton pye

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -7,6 +7,7 @@ import { DibujoPage } from '../dibujo/dibujo';
 import { ComPage } from '../com/com';
 import { RoboticaPage } from '../robotica/robotica';
 import { PappsPage } from '../papps/papps';
+import { PyePage } from '../pye/pye';
 
 @Component({
   selector: 'page-home',
@@ -20,6 +21,7 @@ export class HomePage {
   com = ComPage;
   rob = RoboticaPage;
   papps = PappsPage;
+  pye = PyePage;
   
   constructor(public navCtrl: NavController) {
 
@@ -45,4 +47,8 @@ export class HomePage {
   clickPapps(){
     this.navCtrl.push(this.papps);
   }
+  clickPye(){
+    this.navCtrl.push(this.pye);
+  }
+
 }
